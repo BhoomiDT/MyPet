89f52266e6d4370766624d020a5f23ebad2e80c0
BhoomiDT <tundalwarbhoomi@gmail.com>
2026-02-12 11:19:32 +0530

    Initial Commit

---
+++ MyPet/MyPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPetApp.swift
+//  MyPet
+//
+//  Created by SDC-USER on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MyPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
